c0e769930d40099df54f956b7ed33cf526f5c48e
Paul Northrop <p.northrop@ucl.ac.uk>
2019-02-09 11:51:50 +0000

    Correct links

--- R/box_cox_functions_rcpp.R
+++ R/box_cox_functions_rcpp.R
@@ -74,7 +74,7 @@
 #'  211-252, \url{https://www.jstor.org/stable/2984418}.
 #' @references Andrews, D. F. and Gnanadesikan, R. and Warner, J. L. (1971)
 #'  Transformations of Multivariate Data, Biometrics, 27(4),
-#'  \url{https://dx.doi.org/10.2307/2528821}.
+#'  \href{https://www.jstor.org/stable/2528821?origin=crossref&seq=1#page_scan_tab_contents}{https://dx.doi.org/10.2307/2528821}.
 #' @references Eddelbuettel, D. and Francois, R. (2011). Rcpp: Seamless
 #'  R and C++ Integration. \emph{Journal of Statistical Software},
 #'  \strong{40}(8), 1-18.
@@ -328,7 +328,7 @@ find_lambda_one_d_rcpp <- function(logf, ..., ep_bc = 1e-4, min_phi = ep_bc,
 #'  211-252, \url{https://www.jstor.org/stable/2984418}.
 #' @references Andrews, D. F. and Gnanadesikan, R. and Warner, J. L. (1971)
 #'  Transformations of Multivariate Data, Biometrics, 27(4),
-#'  \url{https://dx.doi.org/10.2307/2528821}.
+#'  \href{https://www.jstor.org/stable/2528821?origin=crossref&seq=1#page_scan_tab_contents}{https://dx.doi.org/10.2307/2528821}.
 #' @references Eddelbuettel, D. and Francois, R. (2011). Rcpp: Seamless
 #'  R and C++ Integration. \emph{Journal of Statistical Software},
 #'  \strong{40}(8), 1-18.
